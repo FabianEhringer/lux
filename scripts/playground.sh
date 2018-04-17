ebd9727f20af64eee044c9c0f203144713329440
Fabian Ehringer <fabian.ehringer@gmx.net>
2018-04-17 20:50:14 +0200

    mkdir installation for playground

--- scripts/playground.sh
+++ scripts/playground.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+mkdir /tmp/installation
 wget https://github.com/graphcool/graphql-playground/releases/download/1.5.3/graphql-playground-electron_1.5.3_amd64.deb -O /tmp/installation/playground.deb
 sudo apt install /tmp/installation/playground.deb
